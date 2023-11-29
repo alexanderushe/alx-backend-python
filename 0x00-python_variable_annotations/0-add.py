f48aa8d18cb55739a047883afd88e031402ec453
alexanderushe <alex.f.ushe@gmail.com>
2023-11-29 04:56:01 +0200

    add type annotated

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+basic annotations - add
+Function to write a type-annotated function that
+takes 2 floats a, b and return the sum of them
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    type-annotated function
+    '''
+    return a + b
